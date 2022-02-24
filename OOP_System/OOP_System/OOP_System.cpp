dc307fba16503f7d79f179e179257a8ae0fb9d08
firstTrip <hdnwls961@gmail.com>
2022-02-25 01:15:19 +0900

    OOP_System v0.6
    자식 클래스 NomalAccount 와 HighCreditAccount 추가 , AccountManager 수정

--- OOP_System/OOP_System/OOP_System.cpp
+++ OOP_System/OOP_System/OOP_System.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+//신용등급 
+enum { Level_A =7, Level_B = 4,Level_C = 2};
 
-class User // Entity 클래스 , 데이터를 저장하고 보관하는 클래스 
+class Account // Entity 클래스 , 데이터를 저장하고 보관하는 클래스 
 {
 private: //정보은닉 
 	const int ID;
@@ -12,7 +14,7 @@ private: //정보은닉
 
 public :
 
-	void ShowInfo() const
+	virtual void ShowInfo() const // virtual 함수를 사용하여 다형성을 사용
 	{
 		cout << "계좌 번호 :" << ID << '\n';
 		cout << "현재 금액 :" << Money << '\n';
@@ -25,7 +27,7 @@ public :
 	}
 
 
-	void Insert(int money)
+	virtual void Insert(int money)
 	{
 		this->Money += money;
 	}
@@ -38,17 +40,98 @@ public :
 		return this->Money;
 	}
 
-	User(int ID, int Money, string Name) : ID(ID), Money(Money), Name(Name)
+
+	Account(int ID, int Money, string Name) : ID(ID), Money(Money), Name(Name)
 	{
 		// 이니셜 라이저
 	}
 
-	User(const User& copy) : ID(copy.ID) , Money(copy.Money), Name(copy.Name)
+	Account(const Account& copy) : ID(copy.ID) , Money(copy.Money), Name(copy.Name)
 	{
 		cout << "new Copy obj :" << endl;
 	}
 
-	~User()
+	~Account() 
+	{
+		// 소멸자
+		cout << "delete obj :" << endl;
+
+	}
+};
+
+class NomalAccount : public Account // Account 를 부모로 둔 자식 클래스 , interst 변수가 추가된다.
+{
+private:
+	const int interest;
+
+
+
+public:
+	NomalAccount(int ID, int Money, string Name ,int interest) : Account(ID,Money,Name), interest(interest)
+	{
+		// 이니셜 라이저
+	}
+	/*
+	NomalAccount(const NomalAccount& copy) :Account(copy.ID, Money, Name), grade(copy.grade)
+	{
+		cout << "new Copy obj :" << endl;
+	}*/
+
+	virtual void ShowInfo() const
+	{
+		Account::ShowInfo();// 부모의 ShowInfo 를 호출 
+		cout << "이자율 :" << interest << '\n';
+
+	}
+
+	virtual void Insert(int money)
+	{
+		Account::Insert(money);
+		Account::Insert(money *(interest/100.0)); // 이자 계산
+
+	}
+
+	~NomalAccount()
+	{
+		// 소멸자
+		cout << "delete obj :" << endl;
+
+	}
+};
+
+class HighCreditAccount : public NomalAccount // NomalAccount를 부모로 사용하는 클래스 추가 이자가 존재한다.
+{
+private:
+	const int specialInterest;
+
+
+
+public:
+	HighCreditAccount(int ID, int Money, string Name,int rate ,int special ) : NomalAccount(ID, Money, Name,rate), specialInterest(special)
+	{
+		// 이니셜 라이저
+	}
+	/*
+	NomalAccount(const NomalAccount& copy) :Account(copy.ID, Money, Name), grade(copy.grade)
+	{
+		cout << "new Copy obj :" << endl;
+	}*/
+
+	virtual void Insert(int money)
+	{
+		Account::Insert(money);
+		Account::Insert(money * (specialInterest / 100.0)); // 이자 계산
+
+	}
+
+	virtual void ShowInfo() const
+	{
+		Account::ShowInfo();
+		cout << "이자율 :" << specialInterest << '\n';
+
+
+	}
+	~HighCreditAccount()
 	{
 		// 소멸자
 		cout << "delete obj :" << endl;
@@ -60,7 +143,7 @@ class AccountHandler // 컨트롤 클래스 추가 , 전반적인 기능을 담당하는 클래스
 {
 private:
 
-	User* user[100];
+	Account* user[100];
 	int idx;
 
 public :
@@ -69,14 +152,14 @@ public :
 	~AccountHandler(); // 소멸자
 
 	void CreateAccount(void);
+	void CreateNomalAccount(void);
+	void CreateHighCreditAccount(void);
 	void ShowAllAccount(void) const; // 값의 변환을 허용하지 않기 때문에 const 를 선언 해준다.
 	void CallInsert(void);
 	void CallRemove(void);
 	void ShowMenu(void);
 	void Menu(int val);
 };
-
-
 
 
 
@@ -110,8 +193,47 @@ void  AccountHandler::Menu(int val)
 
 void  AccountHandler::CreateAccount()
 {
-	int num, money;
+
+	int x;
+	cout << "[계좌종류 선택]" << '\n';
+	cout << "1.보통예금 계좌 2. 신용신뢰 계좌" << '\n';
+	cout << "선택 : ";
+
+	cin >> x;
+
+
+	switch (x)
+	{
+	case 1:
+
+		CreateNomalAccount();
+		break;
+
+	case 2:
+
+		CreateHighCreditAccount();
+		break;
+
+	default:
+
+		cout << "잘못 입력 하셨습니다." << '\n';
+
+		break;
+	}
+
+
+
+	
+}
+
+void  AccountHandler::CreateNomalAccount()
+{
+
+	int num, money, interest;
 	string name;
+
+	cout << "[보통예금 계좌]" << '\n';
+
 
 	cout << "계좌 번호를 입력 해주세요." << '\n';
 	cin >> num;
@@ -119,9 +241,52 @@ void  AccountHandler::CreateAccount()
 	cin >> money;
 	cout << "이름을 입력 해주세요. " << '\n';
 	cin >> name;
+	cout << "이자율 :. " << '\n';
+	cin >> interest;
 
-	user[idx++] = new User(num, money, name);
+	user[idx++] = new NomalAccount(num, money, name, interest);
 }
+
+void  AccountHandler::CreateHighCreditAccount()
+{
+
+	int num, money, interest;
+	string name;
+
+	int grade;
+
+	cout << "[신용신뢰 계좌]" << '\n';
+
+
+	cout << "계좌 번호를 입력 해주세요." << '\n';
+	cin >> num;
+	cout << "현재 금액을 입력 해주세요. " << '\n';
+	cin >> money;
+	cout << "이름을 입력 해주세요. " << '\n';
+	cin >> name;
+	cout << "이자율 :. " << '\n';
+	cin >> interest;
+	cout << "신용 등급(1toA , 2toB, 3toC) :. " << '\n';
+	cin >> grade;
+
+
+	switch (grade)
+	{
+
+	case 1:
+		user[idx++] = new HighCreditAccount(num, money, name, interest, Level_A);
+		break;
+	case 2:
+		user[idx++] = new HighCreditAccount(num, money, name, interest, Level_B);
+		break;
+	case 3:
+		user[idx++] = new HighCreditAccount(num, money, name, interest, Level_C);
+		break;
+	default:
+		break;
+	}
+}
+
 
 void AccountHandler::ShowAllAccount() const
 {
@@ -208,11 +373,15 @@ int main()
 	AccountHandler manager;
 	int val;
 
+
+
 	while (1)
 	{
 		manager.ShowMenu();
 		
+		cout << "번호를 선택 해 주세요 " << '\n';
 
+		cin >> val;
 		switch (val)
 		{
 		case 1:
